5611644c11fb849cb3c7c3c1521f8452add9e463
Rémi Bove <rbove@antidot.net>
2024-04-30 17:41:24 +0200

    Renaming files

---
+++ in-product-help/script.js
@@ -0,0 +1,28 @@
+var iph = document.createElement('script');
+iph.async = true;
+iph.src = 'https://cdn.jsdelivr.net/npm/@fluid-topics/ft-in-product-help/build/ft-in-product-help.min.js';
+document.append(iph);
+
+var b = document.createElement('script');
+b.async = true;
+b.src = 'https://cdn.jsdelivr.net/npm/@fluid-topics/ft-button/build/ft-button.min.js';
+document.append(b);
+
+var sw = document.createElement('script');
+sw.async = true;
+sw.src = 'https://cdn.jsdelivr.net/npm/@fluid-topics/ft-size-watcher/build/ft-size-watcher.min.js';
+document.append(sw);
+
+document.getElementById('toto').addEventListener('click', function() {
+  let iph = document.getElementById('iph');
+  //iph.setAttribute('opened', 'true');
+  console.log(iph.getAttribute('opened'))
+  if (iph.hasAttribute('opened')){
+      iph.removeAttribute('opened');
+  }
+  else{
+      iph.setAttribute('opened', "");
+  }
+
+});
+
